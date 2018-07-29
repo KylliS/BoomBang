817342adc792527de2eb41900490b8a7b5730c11
Külli Siimon <kylli.siimon@outlook.com>
2018-07-29 23:51:40 +0300

    Task 3

---
+++ Boom/Boom/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boom
+{
+    class Program
+    {
+        public static string [] BoomBang(int start,int end)
+        {
+            List<string> Words = new List<string>();
+           
+            for (var i = start; i< end; i++)
+            {
+               if (i % 3 == 0 && i % 5 == 0)
+                Words.Add("BoomBang");
+                 else if (i % 3 == 0)
+                Words.Add("Boom");
+                 else if (i % 5 == 0)
+                Words.Add("Bang");
+                else
+                Words.Add(i.ToString());
+                  
+            }
+            string[] symbols = Words.ToArray();
+            return (symbols);
+         
+        
+        }
+    
+        static void Main(string[] args)
+            {
+            Console.Write("Insert starting number:");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insert ending number:");
+            int end = Convert.ToInt32(Console.ReadLine());
+            string[] array = BoomBang(start, end);
+            Console.WriteLine("[{0}]",string.Join(",",array));
+            Console.ReadLine();
+
+            }
+    }
+   
+}
+
